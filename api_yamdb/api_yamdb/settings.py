7a930bdd83a866f23c1ddaa2248d0d2df3d4f1a6
dexie7 <dexie7@yandex.ru>
2022-09-14 20:48:35 +0500

    My 2 commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,19 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY', default=None)
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -64,7 +60,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
-# Database
 
 DATABASES = {
     'default': {
@@ -81,7 +76,6 @@ DATABASES = {
 AUTH_USER_MODEL = 'reviews.User'
 
 
-# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -114,11 +108,10 @@ SIMPLE_JWT = {
 }
 
 
-# Internationalization
 
 LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -127,7 +120,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -172,12 +164,10 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 FROM_EMAIL = 'ex@yamail.ru'
 
-# USER FIELDS LENGTH
 USERNAME_LENGTH = 150
 EMAIL_LENGTH = 254
 FIRST_NAME_LENGTH = 150
 LAST_NAME_LENGTH = 150
 PASSWORD_LENGTH = 100
 
-# CONFIRMATION_CODE_LENGTH
 CONFIRMATION_CODE_LENGTH = 50
